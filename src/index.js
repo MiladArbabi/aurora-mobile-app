526616c43fa508bfccf21a19429ad8d9f234beaa
Milad <miladarbabi@Milads-MacBook-Pro.local>
2025-03-12 21:48:33 +0100

    Added MongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
 dotenv.config();
+connectDB(); // Connect to MongoDB
+
+const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
